a927f25dadc1439a053dd733b1e3f78a65ada5e7
9pyr <mpdhc.s@gmail.com>
2020-07-31 15:33:45 +0700

    update

---
+++ build/precache-manifest.391292dd057e08bad8db677122a451e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99591173e56695aad8fa64461f5b9b18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6409dac1d45dc084a28",
+    "url": "/static/css/main.d5bc94fa.chunk.css"
+  },
+  {
+    "revision": "45eb45ce1e4b4d30332f",
+    "url": "/static/js/2.0b7a7427.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0b7a7427.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6409dac1d45dc084a28",
+    "url": "/static/js/main.46a45b2f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
